389ef1ecb8fb631f6e651ea0695c729e7dd4e07e
strikertushar19 <tushardogra19@gmail.com>
2025-03-27 12:56:41 +0530

    first  version portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import { motion } from 'framer-motion';
 import {
@@ -12,7 +13,7 @@ import {
     Download,
     Twitter,
 } from 'lucide-react';
-import { cn } from "@/lib/utils"
+import { cn } from "../app/lib/utils/cn";
 
 // Resume Data (based on the provided PDF)
 const resumeData = {
