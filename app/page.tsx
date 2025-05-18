12eed1694d4a2dfbfceb26c43dcf01b56545ae15
Tushar Dogra <tushardogra19@gmail.com>
2025-05-18 20:41:05 +0530

    added new face image

--- app/page.tsx
+++ app/page.tsx
@@ -476,7 +476,7 @@ const App = () => {
                         <div className="absolute inset-0 bg-gradient-to-tr from-indigo-500 to-purple-500 rounded-full blur-xl opacity-20"></div>
                         <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white shadow-2xl">
                             <img
-                                src="/face.jpg"
+                                src="/face2.jpeg"
                                 alt="Tushar Dogra"
                                 className="w-full h-full object-cover"
                             />
